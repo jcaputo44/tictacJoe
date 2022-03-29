654802da9dc51d23284f06b098279f7907c6efad
username <josephcaputo@Josephs-MacBook-Pro.local>
2022-03-28 17:49:33 -0700

    final

--- mainjs.js
+++ mainjs.js
@@ -1,11 +1,23 @@
 /*----- constants -----*/
 
+// const winningCombos = [
+//   [0,1,2]
+//   [3,4,5]
+//   [6,7,8]
+//   [0,3,6]
+//   [1,4,7]
+//   [2,5,8]
+//   [0,4,8]
+//   [2,4,6]
+// ]
+
 
 /*----- app's state (variables) -----*/
 
 
 let player = "x";
 let gameStatus;
+let board;
 
 
 /*----- cached element references -----*/
@@ -17,15 +29,12 @@ const resetBtn = document.getElementById('reset-btn');
 /*----- event listeners -----*/
 
  
-// document.querySelector('button')
-//   .addEventListener('click', init() );
-
 resetBtn.addEventListener('click', init);
 
     
-    document.querySelector('section')
+  document.querySelector('section')
     .addEventListener('click', function(evt) {
-      // const idx = parseInt(evt.target.id);
+    // const idx = parseInt(evt.target.id);
       const idx = squareEls.indexOf(evt.target);
       if (idx === -1) return;
       // console.log(idx);
@@ -36,16 +45,19 @@ resetBtn.addEventListener('click', init);
     init (); 
 
     function init() {
+      squareEls.forEach(function(squareEl){
+        squareEl.innerText = "";
+      }) 
       player = 'x';
+      board = [null, null, null, null, null, null, null, null, null];
+      // div = '';
+      render();
      }
 
-    render();
-
-    function render() {
-      squareEls.innerText = '';
+  function render() {
+      // squareEls.innerText = '';
     }
   
-    
     squareEls.forEach(function(squareEl) {
       squareEl.addEventListener('click', function(){
         if(squareEl.innerText != '') return;
@@ -53,13 +65,3 @@ resetBtn.addEventListener('click', init);
         player = (player === "x" ? "o" : "x");
      })
     });
-
-
-
-   
-
-
-
-
-
-
